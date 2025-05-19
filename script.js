21e1d81d0282b94a25bc9c9756a4b9063ca88288
Humble Dev <abdulraufabdulhakim71@gmail.com>
2025-05-19 00:20:53 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+const FeaturesSection=document.getElementById('Features')
+const MaterialsSection=document.getElementById('Materials')
+const AboutSection=document.getElementById('About')
+const ContactSection=document.getElementById('Contact-sect')
+
+const CEOsection=document.getElementById('About-CEO');
+const ServiesBox=document.getElementById('service')
+
+
+const menuBox=document.getElementById('menu');
+
+
+// function Service(){
+// FeaturesSection.style.display='none'
+// MaterialsSection.style.display='none'
+// AboutSection.style.display='none'
+// ContactSection.style.display='none'
+// CEOsection.style.display='none'
+// ServiesBox.style.display='block'
+
+
+// }
+
+
+function menu(){
+   menuBox.style.display='inline'
+}
+
+function clean(){
+   menuBox.style.display='none'
+}
